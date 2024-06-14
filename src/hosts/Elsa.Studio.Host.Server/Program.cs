ae62d61f50237442f338aa07484f67cf2ac88a2d
Ghanshyam Dhanani <ghanshyam.dhanani@outamation.com>
2024-06-13 21:31:16 -0400

    Issue fix

--- src/hosts/Elsa.Studio.Host.Server/Program.cs
+++ src/hosts/Elsa.Studio.Host.Server/Program.cs
@@ -47,13 +47,12 @@ builder.Services.AddSignalR(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-/*if (!app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
-*//*    app.UseResponseCompression();
-*//*
+    //app.UseResponseCompression();
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}*/
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
